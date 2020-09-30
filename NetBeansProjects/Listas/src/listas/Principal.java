4947aaeb94e2998df49d3b3e27134668be87e778
pabpan <pabpan@alu.iesserpis.org>
2020-09-30 13:49:23 +0200

    Actualiación

--- NetBeansProjects/Listas/src/listas/Principal.java
+++ NetBeansProjects/Listas/src/listas/Principal.java
@@ -1,12 +1,10 @@
-
 package listas;
 
 public class Principal {
-    
+
     public static void main(String[] args) {
-        
+
         Listas lista1 = new Listas();
         lista1.setVisible(true);
     }
-    
 }
