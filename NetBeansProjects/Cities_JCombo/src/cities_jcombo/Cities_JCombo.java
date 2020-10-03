7d19d0a7af0269c90798716a73a7a09ce3d8a8b7
pabpan <pabpan@alu.iesserpis.org>
2020-10-03 13:44:53 +0200

    Combo Ciudades

---
+++ NetBeansProjects/Cities_JCombo/src/cities_jcombo/Cities_JCombo.java
@@ -0,0 +1,38 @@
+package cities_jcombo;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Cities_JCombo extends JFrame {
+
+    final JPanel panel = new JPanel();
+    //final GridLayout layout = new GridLayout(0, 3);
+    JLabel etiqueta_roja = new JLabel("Select your city");
+    final String ciudades[] = {"Roma", "New York", "Tokio", "Moscou"};
+    final JComboBox <String> lista_combobox = new JComboBox(ciudades);
+    JLabel etiqueta_azul = new JLabel("Where do you want to go?");
+
+    public Cities_JCombo() {
+
+        setSize(600, 80);
+        setTitle("Cities I would like to visit when the Pandemic is over");
+        panel.add(etiqueta_roja);
+        etiqueta_roja.setForeground(Color.red);
+        lista_combobox.addActionListener(new Gestion_combobox());
+        panel.add(lista_combobox);
+        etiqueta_azul.setForeground(Color.blue);
+        panel.add(etiqueta_azul);
+        add(panel);
+    }
+
+    private class Gestion_combobox implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            int indice = lista_combobox.getSelectedIndex();
+            String aux = ciudades[indice];
+            etiqueta_azul.setText(aux + " is selected.");
+        }
+    }
+}
---
+++ NetBeansProjects/Cities_JCombo/src/cities_jcombo/Principal.java
@@ -0,0 +1,11 @@
+package cities_jcombo;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Cities_JCombo nuevo = new Cities_JCombo();
+        nuevo.setVisible(true);
+        
+    }
+}
