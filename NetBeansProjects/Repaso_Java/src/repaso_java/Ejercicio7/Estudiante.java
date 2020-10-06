37d2196bdcddbd9aea516a53fad628e8341e3e94
Pablo <pabpan@alu.iesserpis.org>
2020-10-06 13:10:11 +0200

    Actualizado 10/06/20 - 13:10

---
+++ NetBeansProjects/Repaso_Java/src/repaso_java/Ejercicio7/Estudiante.java
@@ -0,0 +1,15 @@
+package repaso_java.Ejercicio7;
+
+public class Estudiante {
+
+    private String Nombre;
+    private int Edad;
+
+    public Estudiante (String Nombre, int Edad) {
+
+        this.Nombre = Nombre;
+        this.Edad = Edad;
+
+    }
+
+}
---
+++ NetBeansProjects/Repaso_Java/src/repaso_java/Ejercicio7/Principal.java
@@ -0,0 +1,10 @@
+package repaso_java.Ejercicio7;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        Estudiante nuevo = new Estudiante ();
+    }
+    
+}
