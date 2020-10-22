1c367206f877c42b4426c48771ccc5d3a728d9a9
pabpan <pabpan@alu.iesserpis.org>
2020-10-22 11:00:21 +0200

    Actualizado 22/10/20 - 11:00

---
+++ NetBeansProjects/ManejoJDBC/src/main/java/test/TestManejoPersonas.java
@@ -0,0 +1,31 @@
+package test;
+
+import datos.personaDAO;
+import domain.Persona;
+import java.sql.SQLException;
+import java.util.*;
+
+public class TestManejoPersonas {
+
+    public static void main(String[] args) throws SQLException {
+        personaDAO personaDao = new personaDAO();
+        List<Persona> personas = personaDao.seleccionar();
+        
+        //CREAR NUEVO REGISTRO
+        //Persona personaNueva = new Persona("Paco", "Pérez", 34);
+        //personaDao.insertar(personaNueva);
+        
+        //ACTUALIZAR NUEVO REGISTRO
+        //Persona modificar_persona = new Persona(1, "fgasdfsda", "fasdgsag", 89);
+        //personaDao.update(modificar_persona);
+        
+        //BORRAR REGISTRO
+        Persona borrar_persona = new Persona(2);
+        personaDao.delete(borrar_persona);
+        
+        personas = personaDao.seleccionar();
+        personas.forEach(persona -> {
+            System.out.println("persona = " + persona);
+        });
+    }
+}
