8b0189f8474ff39b12a570bb5b07ecce3b690100
pabpan <pabpan@alu.iesserpis.org>
2020-11-08 23:44:11 +0100

    Actualizado 08/11/20 - 23:44

--- NetBeansProjects/Practica_JOption_Pane_Galore/src/practica_joption_pane_galore/Practica_JOption_Pane_Galore.java
+++ NetBeansProjects/Practica_JOption_Pane_Galore/src/practica_joption_pane_galore/Practica_JOption_Pane_Galore.java
@@ -3,6 +3,7 @@ package practica_joption_pane_galore;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -31,6 +32,8 @@ public class Practica_JOption_Pane_Galore extends JFrame {
         boton_error.addActionListener(new Error());
         boton_ok.addActionListener(new Confirmacion());
         boton_texto.addActionListener(new Texto());
+        boton_combo.addActionListener(new Combo());
+        boton_mas.addActionListener(new Mas());
 
         panel_global.add(boton_info);
         panel_global.add(boton_adv);
@@ -47,7 +50,6 @@ public class Practica_JOption_Pane_Galore extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent arg0) {
-            JOptionPane.showMessageDialog(null, "Este es un mensaje simple tipo información", "Mensaje", JOptionPane.INFORMATION_MESSAGE);
             JOptionPane.showMessageDialog(null, "Este es un mensaje simple tipo información", "Mensaje", JOptionPane.INFORMATION_MESSAGE);
         }
     }
@@ -85,8 +87,48 @@ public class Practica_JOption_Pane_Galore extends JFrame {
                 int resultado = 2 * Integer.parseInt(texto);
                 JOptionPane.showMessageDialog(null, "El resultado es: " + texto + "*2 = " + resultado, "Mensaje", JOptionPane.INFORMATION_MESSAGE);
             } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "El valor ingresado debe ser numérico", "Mensaje", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "El valor ingresado debe ser numérico", "ERROR", JOptionPane.ERROR_MESSAGE);
             }
         }
+    }
+
+    public class Combo implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            Object seleccion = JOptionPane.showInputDialog(null, "Selecciones un color", "COLORES", JOptionPane.QUESTION_MESSAGE,
+                    null, new Object[]{"Seleccione", "Rojo", "amarillo", "Azul"}, "Seleccione");
+            if (seleccion != null && seleccion != "Seleccione") {
+                JOptionPane.showMessageDialog(null, "Color Seleccionado " + seleccion, "Mensaje", JOptionPane.INFORMATION_MESSAGE);
+            }
+            if (seleccion == "Seleccione") {
+                JOptionPane.showMessageDialog(null, "No ha seleccionado ningún color", "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    public class Mas implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            JCheckBox opcion1 = new JCheckBox("A veces");
+            JCheckBox opcion2 = new JCheckBox("A menudo");
+            JCheckBox opcion3 = new JCheckBox("Siempre");
+            Object[] opciones = {opcion1, opcion2, opcion3};
+
+            int seleccion = JOptionPane.showOptionDialog(null, "Seleccione una opcion", "Selector de opciones", JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
+            if (opcion1.isSelected()) {
+                JOptionPane.showMessageDialog(null, "Seleccionado a veces", "A veces", JOptionPane.INFORMATION_MESSAGE);
+            }
+            if (opcion2.isSelected()) {
+                JOptionPane.showMessageDialog(null, "Seleccionado a menudo", "A menudo", JOptionPane.INFORMATION_MESSAGE);
+            }
+            if (opcion3.isSelected()) {
+                JOptionPane.showMessageDialog(null, "Seleccionado siempre", "Siempre", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+
     }
 }
