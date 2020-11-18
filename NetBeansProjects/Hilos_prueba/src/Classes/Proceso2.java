c3ab83f7a84decf6286cfb84b6d18a4124c9af26
pabpan <pabpan@alu.iesserpis.org>
2020-11-18 21:20:27 +0100

    Actualizado 18/11/20 - 21:20

---
+++ NetBeansProjects/Hilos_prueba/src/Classes/Proceso2.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class Proceso2 implements Runnable {
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Proceso 2");
+        }
+
+    }
+}
